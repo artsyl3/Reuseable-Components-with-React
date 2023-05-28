48212f679c2a5dfe27c1343f261465625a2ead66
artsyl3 <118113234+artStyl3@users.noreply.github.com>
2023-05-28 14:02:58 +0200

    changes

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,21 @@
-function Table({ data , config , keyFn }) {
+function Table({ data, config, keyFn }) {
+  const renderedHeaders = config.map((column) => {
+    if (column.header) {
+      return column.header();
+    }
+
+    return <th key={column.label}>{column.label}</th>;
+  });
+
   const renderedRows = data.map((rowData) => {
     const renderedCells = config.map((column) => {
-      return (<td className="p-3">{column.render(rowData)}</td>);
+      return (
+        <td className="p-2" key={column.label}>
+          {column.render(rowData)}
+        </td>
+      );
     });
-    
+
     return (
       <tr className="border-b" key={keyFn(rowData)}>
         {renderedCells}
@@ -11,14 +23,10 @@ function Table({ data , config , keyFn }) {
     );
   });
 
-  const renderedColumns = config.map((column) => {
-    return <th key={column.label}>{column.label}</th>
-  })
-
   return (
     <table className="table-auto border-spacing-2">
       <thead>
-        <tr className="border-b-2">{renderedColumns}</tr>
+        <tr className="border-b-2">{renderedHeaders}</tr>
       </thead>
       <tbody>{renderedRows}</tbody>
     </table>
